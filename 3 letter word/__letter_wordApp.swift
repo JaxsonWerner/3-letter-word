f518b4c5bc9f610d99a54dbd7ca4e486bd2fcaf3
Jaxson <Werner>
2025-11-04 10:31:54 -0600

    Initial Commit

---
+++ 3 letter word/__letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  __letter_wordApp.swift
+//  3 letter word
+//
+//  Created by Jaxson Werner on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct __letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
